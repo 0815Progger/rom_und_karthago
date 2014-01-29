b3535dda205503c7c09739f59fd2d778f23ced8b
michael <michael@acer-aspire-one>
2014-01-29 17:26:33 +0100

    Herrscher der Sieben Weltmächte

--- src/data/AI.scala
+++ src/data/AI.scala
@@ -8,6 +8,14 @@ package data
 class AI extends Spieler("AI") {
   //var name = "AI"
   var schwierigkeitsgrad: String = "test"
+
+def schreibWas = {
+  println("Das hier ist die total schlaue A.I")
 }
 
+/*
+ * Das hier ist ein furchtbar wichtiger Kommentar! Trust me, iam an engineer!
+ * Hier endet der furchtbar wichtige Kommentar. TRUST ME!!!
+ */
+}
 //Test
