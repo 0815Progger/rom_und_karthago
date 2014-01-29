984a2cdf03360b4cb50f7cd43710c4eb75942d76
Joones <joones333ok@googlemail.com>
2014-01-29 17:37:44 +0100

    try out again

--- src/data/AI.scala
+++ src/data/AI.scala
@@ -8,6 +8,7 @@ package data
 class AI extends Spieler("AI") {
   //var name = "AI"
   var schwierigkeitsgrad: String = "test"
+  var clever:Boolean = true
     
   def intelligent:Boolean={
     println("not intelligent yet")
